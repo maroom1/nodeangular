8cc3f000819074ffd40ee818543446c6a3cddafc
maroom1 <godugulurigmk@gmail.com>
2018-07-11 20:44:57 -0700

    FormModule included

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -33,11 +33,23 @@ export class UserComponent implements OnInit {
   onClick(){
     console.log("on click me button");
     if(this.name == "Tiger")
-       this.name="Lion"
+       this.name="Lion";
     else
-      this.name="Tiger"
-      this.hobbies.push("NewHobby")
-    
+      this.name="Tiger";
+      this.hobbies.push("NewHobby");
+  }
+
+  addHobby(hobby){
+    console.log(hobby);
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+  deleteHobby(hobby){
+    for(let i = 0;i<this.hobbies.length;i++){
+      if(this.hobbies[i]==hobby){
+        this.hobbies.splice(i,1);
+      }
+    }
   }
 }
 
